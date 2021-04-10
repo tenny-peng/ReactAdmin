693c9e4fe9519e2927d12e611f0c9e7e85359552
tenny <mpengtaoqi@163.com>
2021-04-10 16:31:53 +0800

    antd optimize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
+import './App.less'
 import { Button } from 'antd'
-import 'antd/dist/antd.css'
 
 export default class App extends Component {
   render() {
